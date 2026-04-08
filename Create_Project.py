8b25c4ebd6a3ebe2fbc2112a486805fcdb88e37d
aSpaceToaster <agreenproto@gmail.com>
2026-04-07 20:29:53 -0400

    Add project state editing, add more comments

--- Create_Project.py
+++ Create_Project.py
@@ -6,13 +6,15 @@ from tkinter import ttk
 import os
 import platform
 
+# SETUP ###########################################################################################
 
-# define constants
+# Define constants
 FILE_EXT = ".txt"
 TASK_STATE_OPTIONS = [ "C  - Complete", "IP - In Progress", "TD - To Do" ]
 SYSTEM = platform.system()
 IS_PI = platform.platform().count( "rpi" ) >= 1
 
+# Determine os, Define file locations
 if( SYSTEM == "Windows" ):
 	PROJECT_DIRECTORY = "C:\\Users\\Space Toaster\\Documents\\Projects"
 	if( not os.path.isdir( PROJECT_DIRECTORY ) ):
@@ -38,7 +40,7 @@ else:
 	print( "Please run this code on windows, mac os, or linux")
 	quit()
 
-
+# Setup for raspberry pi
 if( IS_PI	):
 	try:
 		from gpiozero import *
@@ -50,11 +52,9 @@ if( IS_PI	):
 		testPinOut.off()
 	except Exception as e:
 		IS_PI = False
-	
 
 
-
-# init vars
+# Init vars
 tasks = []
 taskStates = []
 materials = []
@@ -65,21 +65,32 @@ title = []
 state = []
 currTask = ""
 
-# reads project names from folder
+
+
+# FUNCTIONS ####################################################################################### 
+
+# PROJECTS ----------------------------------------------------------------------------------------
+
+# Reads project names from folder
 def readProjects():
+  # Find folder contents
 	projectFolder = os.listdir( PROJECT_DIRECTORY )
+	
+	# Remove non-.txt files from list
 	for project in projectFolder:
 		if project.count( FILE_EXT ) == 0:
 			projectFolder.remove( project )
+  
+  # Update variables and GUI
 	projects = projectFolder
 	projectNames.clear()
 	for project in projects:
 		projectNames.append( project[ 0 : project.index( ".txt" ) ] )
 
-# read the information stored in a project file, store it in lists
+# Read the information stored in a project file, store it in lists
 def interpretFile( filename ):
 	try:
-		# clear vars
+		# Clear vars
 		state.clear()
 		title.clear()
 		tasks.clear()
@@ -87,46 +98,53 @@ def interpretFile( filename ):
 		materials.clear()
 		materialQtys.clear()
 
-		# read file contents
+		# Read file contents
 		contents = open( filename ).read()
 		state.append( contents.splitlines()[ 1 ] )
 		title.append( contents[ contents.index( 'NAME' ) + 6 : contents.index( 'TASKS' ) - 2 ] )
 		taskSect = contents[ contents.index( 'TASKS' ) + 7 : contents.index( 'MATERIALS' ) - 2].splitlines()
 		materialSect = contents[ contents.index( 'MATERIALS' ) + 11 : len( contents ) ].splitlines()
 
-		# parse tasks and task states
+		# Parse tasks and task states
 		if taskSect:
 			for line in taskSect:
 				tasks.append( line[ 0 : line.index( ' - ' ) ] )
 				taskStates.append( line[ line.index( ' - ' ) + 3 : len( line ) ] )
 
-		# parse materials and material quantities
+		# Parse materials and material quantities
 		if materialSect:
 			for line in materialSect:
 				materials.append( line[ 0 : line.index( ' - ' ) ] )
 				materialQtys.append( line[ line.index( ' - ' ) + 3 : len( line ) ] )
 
+		# Save state and title to StringVars
 		if state and title:
 			projState.set( state[ 0 ] )
 			projTitle.set( title[ 0 ] )
 
 	except OSError as e:
+		# Thrown if selected file is not found
 		print( "File not found exception: " + str(e) )
 	except ValueError:
+		# Thrown if selected file does not contain project file formatting
 		print( "File not a project file" )
 	except Exception as e:
+		# Catch-all error case
 		print( e )
 
 # updates vars and GUI on project select
 def selectProject():
+  # Select and read file
 	projectName = currentProject.get()
 	interpretFile( PROJECT_DIRECTORY + projectName + FILE_EXT )
 	currProj.config( text="Editing: " + projectName )
+	
 	# set materials
 	for item in materialList.get_children():
 		materialList.delete( item )
 	for material, qty in zip( materials, materialQtys ):
 		materialList.insert( "", "end", values=( material, qty ) )
+  
 	# set tasks
 	for item in taskList.get_children():
 		taskList.delete( item )
@@ -135,6 +153,7 @@ def selectProject():
 
 # creates a new project file
 def newProject():
+	# Create input GUI
 	inputWindow = Toplevel( root )
 	inputWindow.geometry( "300x200" )
 	inputWindow.title( "New Project" )
@@ -144,6 +163,8 @@ def newProject():
 	ttk.Label( inputWindow, text="Project Name" ).pack()
 	ttk.Label( inputWindow, text="Must be a valid file name" ).pack()
 	ttk.Entry( inputWindow, textvariable=projName ).pack()
+	
+	# Saves new project
 	def saveProj():
 		# create and set up new project file
 		file = open( PROJECT_DIRECTORY + projName.get() + ".txt", mode="x" )
@@ -153,6 +174,7 @@ def newProject():
 		file.write( "MATERIALS:" )
 		file.close()
 
+		# Selects new project
 		readProjects()
 		dropdown.set_menu( projectNames[0], *projectNames )
 		currentProject.set( projName.get() )
@@ -160,35 +182,38 @@ def newProject():
 		inputWindow.destroy()
 	ttk.Button( inputWindow, text="Save", command=saveProj ).pack()
 
-# saves edited project to file
+# Saves edited project to file
 def save():
-	# open file
+	# Open file to write to
 	file = open( PROJECT_DIRECTORY + currentProject.get() + ".txt", mode="w" )
-
-	# project name and state
+	
+	# Write project name and state
 	file.write( "STATE:\n" + projState.get() + "\n\n" )
 	file.write( "NAME:\n" + projTitle.get() + "\n\n" )
-
-	# tasks
+	
+	# Write tasks
 	file.write( "TASKS:\n " )
 	for task, state in zip( tasks, taskStates ):
 		file.write( task + " - " + state + "\n" )
-
-	# materials
+	
+	# Write materials
 	file.write( "\nMATERIALS:\n" )
 	for material, qty in zip( materials, materialQtys ):
 		file.write( material + " - " + qty + "\n" )
-
+	
+	# Close file
 	file.close()
 
-# removes a project
+# Removes a project
 def deleteProject():
+  # Create input GUI
 	inputWindow = Toplevel( root )
 	inputWindow.transient( root )
 	inputWindow.grab_set()
 	inputWindow.focus_force()
 	ttk.Label( inputWindow, text="Delete this project?" ).pack()
-
+	
+	# Delete project, clear GUI
 	def deleteProjectForRealz():
 		filePath = PROJECT_DIRECTORY + currentProject.get() + ".txt"
 		if os.path.exists( filePath ):
@@ -205,14 +230,40 @@ def deleteProject():
 			taskList.delete( item )
 		inputWindow.destroy()
 
+	# Cancel project deletion
 	def cancel():
 		inputWindow.destroy()
 
 	ttk.Button( inputWindow, text="Yes", command=deleteProjectForRealz ).pack()
 	ttk.Button( inputWindow, text="No", command=cancel ).pack()
 
-# adds a task to the list
+# change a project state
+def editProject():
+  # Create input GUI
+	inputWindow = Toplevel( root )
+	newState = StringVar( root )
+	inputWindow.geometry( "300x200" )
+	inputWindow.title( "Change Project State" )
+	inputWindow.transient( root )
+	inputWindow.grab_set()
+	inputWindow.focus_force()
+	stateFrame = ttk.Frame( inputWindow )
+	stateFrame.pack()
+	ttk.Label( stateFrame, text="New State:" ).pack( side="left" )
+	ttk.OptionMenu( stateFrame, newState, TASK_STATE_OPTIONS[ 0 ], *TASK_STATE_OPTIONS ).pack( side="left" )
+	
+	# Save project state
+	def save():
+		projState.set( newState.get()[ 0:2 ] )
+		projectState.config( text="\tProject State: " + projState.get() )
+		inputWindow.destroy()
+	ttk.Button( inputWindow, text="Save", command=save ).pack()
+
+# TASKS -------------------------------------------------------------------------------------------
+
+# Adds a task to the list
 def addTask():
+  # Create input GUI
 	inputWindow = Toplevel( root )
 	inputWindow.geometry( "300x200" )
 	inputWindow.title( "Add a Task" )
@@ -223,6 +274,8 @@ def addTask():
 	nameFrame.pack()
 	ttk.Label( nameFrame, text="Task Name:" ).pack( side="left" )
 	ttk.Entry( nameFrame, textvariable=task ).pack( side="left" )
+	
+	# Save new task
 	def saveTask():
 		tasks.append( task.get() )
 		taskStates.append( "TD" )
@@ -230,7 +283,7 @@ def addTask():
 		inputWindow.destroy()
 	ttk.Button( inputWindow, text="Save", command=saveTask ).pack()
 
-#removes a task from the list
+# Removes a task from the list
 def removeTask():
 	selected = taskList.selection()
 	if selected:
@@ -239,9 +292,10 @@ def removeTask():
 		taskStates.pop( idx )
 		taskList.delete( taskList.get_children()[ idx ] )
 
-# changes a task state
+# Changes a task state
 def editTask():
 	if taskList.selection():
+		# Create input GUI
 		currState.set( "TD" )
 		inputWindow = Toplevel( root )
 		inputWindow.geometry( "300x200" )
@@ -254,14 +308,19 @@ def editTask():
 		stateFrame.pack()
 		ttk.Label( stateFrame, text="Task State:" ).pack( side="left" )
 		ttk.OptionMenu( stateFrame, currState, TASK_STATE_OPTIONS[0], *TASK_STATE_OPTIONS ).pack( side="left" )
+
+		# Save task state
 		def save():
 			taskStates[ taskList.index( taskList.selection()[0] ) ] = currState.get()[ 0:2 ]
 			taskList.set( taskList.selection(), "State", value=currState.get()[ 0:2 ] )
 			inputWindow.destroy()
 		ttk.Button( inputWindow, text="Save", command=save ).pack()
 
-# adds a material and its corresponding quantity
+# MATERIALS ---------------------------------------------------------------------------------------
+
+# Adds a material and its corresponding quantity
 def	addMaterial():
+  # Create input GUI
 	inputWindow = Toplevel( root )
 	inputWindow.geometry( "300x200" )
 	inputWindow.title( "Add a Material" )
@@ -277,6 +336,8 @@ def	addMaterial():
 	ttk.Label( qtyInFrame, text="Quantity:" ).pack( side="left" )
 	ttk.Entry( qtyInFrame, textvariable=currQty ).pack( side="left" )
 	ttk.Label( inputWindow, text="Qty: num/weight/\"-1\" for NA/\"-2\" for undetermined" ).pack()
+	
+	# Save material and quantity
 	def save():
 		materials.append( material.get() )
 		materialQtys.append( currQty.get() )
@@ -284,7 +345,7 @@ def	addMaterial():
 		inputWindow.destroy()
 	ttk.Button( inputWindow, text="Save", command=save ).pack()
 
-# removes a material from the list
+# Removes a material from the list
 def removeMaterial():
 	selected = materialList.selection()
 	if selected:
@@ -293,9 +354,10 @@ def removeMaterial():
 		materialQtys.pop( idx )
 		materialList.delete( materialList.get_children()[ idx ] )
 
-# edit a material quantity
+# Edit a material quantity
 def editMaterial():
 	if materialList.selection():
+		# Create input GUI
 		currQty.set( materialQtys[ materialList.index( materialList.selection()[0] ) ] )
 		inputWindow = Toplevel( root )
 		inputWindow.geometry( "300x200" )
@@ -308,6 +370,8 @@ def editMaterial():
 		ttk.Label( qtyInFrame, text="New Quantity: " ).pack( side="left" )
 		ttk.Entry( qtyInFrame, textvariable=currQty).pack( side="left" )
 		ttk.Label( inputWindow, text="Qty: num/weight/\"-1\" for NA/\"-2\" for undetermined" ).pack()
+
+		# Save material quantity
 		def save():
 			materialQtys[ materialList.index( materialList.selection()[0] ) ] =	currQty.get()
 			materialList.set( materialList.selection()[0], "Quantity", value= currQty.get() )
@@ -315,11 +379,13 @@ def editMaterial():
 		ttk.Button( inputWindow, text="Save", command=save ).pack()
 
 
-# read project filenames
+# MAIN CODE #######################################################################################
+
+# Read project filenames
 readProjects()
 
 
-# Create GUI window
+# Create GUI window, change theme based on OS
 root = Tk()
 root.geometry( "650x450" )
 root.minsize( width=550, height=350 )
@@ -350,30 +416,42 @@ projState = StringVar( root )
 projTitle = StringVar( root )
 
 
-# Project selection tools
+# Project tools
 projToolFrame = ttk.Frame( mainFrame )
 projToolFrame.pack()
 projSelectFrame = ttk.Frame( projToolFrame )
 projSelectFrame.pack()
 projButtonFrame = ttk.Frame( projToolFrame )
 projButtonFrame.pack()
+projInfoFrame = ttk.Frame( projToolFrame )
+projInfoFrame.pack()
+
+# Select, New
 dropdown = ttk.OptionMenu( projSelectFrame, currentProject, currentProject.get(), *projectNames )
 dropdown.pack( side="left", expand=True )
 select = ttk.Button( projSelectFrame, text="Select Project", command=selectProject )
 select.pack( side="left", expand=True )
-newProj = ttk.Button( projButtonFrame, text="New Project", command=newProject )
+newProj = ttk.Button( projSelectFrame, text="New Project", command=newProject )
 newProj.pack( side="left", expand=True )
+
+# Project control
+edtProject = ttk.Button( projButtonFrame, text="Change State", command=editProject )
+edtProject.pack( side="left", expand=True )
 saveProj = ttk.Button( projButtonFrame, text="Save", command=save )
 saveProj.pack( side="left", expand=True )
 delProj = ttk.Button( projButtonFrame, text="Delete Project", command=deleteProject )
-delProj.pack(side="left", expand=True )
-quitBtn = ttk.Button( projButtonFrame, text="Quit Editor", command=quit)
+delProj.pack( side="left", expand=True )
+quitBtn = ttk.Button( projButtonFrame, text="Quit Editor", command=quit )
 quitBtn.pack( side="left", expand=True )
-currProj = ttk.Label( projToolFrame, text="Editing: -----" )
-currProj.pack()
+
+# Current project info
+currProj = ttk.Label( projInfoFrame, text="Editing: -----" )
+currProj.pack( side="left" )
+projectState = ttk.Label( projInfoFrame, text="\tProject State: --" )
+projectState.pack( side="right" )
 
 
-# material and material quantity display
+# Material and material quantity display
 materialFrame = ttk.Frame( mainFrame )
 materialFrame.pack( side="left", fill="both", expand=True, padx=10, pady=10 )
 materialListFrame = ttk.Frame( materialFrame )
@@ -390,6 +468,7 @@ materialList.configure( yscroll=materialScrollbar.set )
 materialScrollbar.pack( side="right", fill="y" )
 materialList.pack( fill="both", expand=True )
 
+# Material control buttons
 materialButtonFrame = ttk.Frame( materialFrame )
 materialButtonFrame.pack()
 newMaterial = ttk.Button( materialButtonFrame, text="Add Material", command=addMaterial )
@@ -400,7 +479,7 @@ edtMaterial = ttk.Button( materialButtonFrame, text="Edit Quantity", command=edi
 edtMaterial.pack( side="left", expand=True )
 
 
-# Task list and task editing buttons
+# Task list
 taskFrame = ttk.Frame( mainFrame )
 taskFrame.pack( side="left", fill="both", expand=True, padx=10, pady=10 )
 taskListFrame = ttk.Frame( taskFrame )
@@ -417,6 +496,7 @@ taskList.configure( yscroll=taskScrollbar.set )
 taskScrollbar.pack( side="right", fill="y" )
 taskList.pack( fill="both", expand=True )
 
+# Task control buttons
 taskButtonFrame = ttk.Frame( taskFrame )
 taskButtonFrame.pack()
 newTask = ttk.Button( taskButtonFrame, text="Add Task", command=addTask )
@@ -426,4 +506,6 @@ delTask.pack( side="left" )
 edtTask = ttk.Button( taskButtonFrame, text="Edit Task", command=editTask)
 edtTask.pack(	side="left" )
 
+
+# Keep GUI running until closed or quit button is pressed
 root.mainloop()
